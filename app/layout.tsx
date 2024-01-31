1e9050d102ee4f5c0a2fb0df856cdbc44d5a92ff
ReD <baslilokmane04@gmail.com>
2024-01-31 03:37:10 +0100

    bug : updating env variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Coming Soon Page",
+  title: "Coming Soon",
   description: "Join the waitlist for our upcoming launch!",
 };
 
