0e25d1dbfef7884299411340854ef97f1bb13a35
ReD <baslilokmane04@gmail.com>
2024-02-03 03:45:30 +0100

    feat : adding faq route

--- app/(prelaunch)/faq/page.tsx
+++ app/(prelaunch)/faq/page.tsx
@@ -1,31 +1,40 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import useGetfaq, { faqType } from "@/app/hooks/useGetfaq";
 const FaqPage = () => {
+  const faqs = useGetfaq();
   return (
     <>
-      <section className="bg-white  bg-inherit">
+      {/* 
+        <h1 class="text-2xl font-semibold text-center text-gray-800 lg:text-3xl dark:text-white">Frequently asked questions</h1>
+
+        <div class="mt-12 space-y-8">
+            <div class="border-2 border-gray-100 rounded-lg dark:border-gray-700">
+            </div>
+        </div> */}
+      <section className="bg-white dark:bg-gray-900 overflow-x-hidden">
         <div className="container max-w-4xl px-6 py-10 mx-auto">
           <h1 className="text-2xl font-semibold text-center  lg:text-3xl dark:text-white">
             Frequently asked questions
           </h1>
 
           <div className="mt-12 space-y-8">
-            <div className="border-2 border-gray-100 rounded-lg ">
-              <div className="flex items-center justify-between w-full p-8">
-                <h1 className="font-semibold text-gray-700 dark:text-white">
-                  How i can play for my appoinment ?
-                </h1>
-              </div>
+            {faqs.map((faq: faqType) => {
+              return (
+                <div className="border-2 border-gray-100 rounded-lg ">
+                  <div className="flex items-center justify-between w-full p-8">
+                    <h1 className="font-semibold text-gray-700 dark:text-white">
+                      {faq.question}
+                    </h1>
+                  </div>
 
-              <hr className="border-gray-200 dark:border-gray-700" />
+                  <hr className="border-gray-200 dark:border-gray-700" />
 
-              <p className="p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Voluptas eaque nobis, fugit odit omnis fugiat deleniti animi ab
-                maxime cum laboriosam recusandae facere dolorum veniam quia
-                pariatur obcaecati illo ducimus?
-              </p>
-            </div>
+                  <p className="p-8 text-sm text-gray-500 dark:text-gray-300">
+                    {faq.answer}
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
---
+++ app/hooks/useGetfaq.ts
@@ -0,0 +1,104 @@
+export type faqType = {
+  id: number;
+  question: string;
+  answer: string;
+};
+
+export default function useGetfaq() {
+  const faq: faqType[] = [
+    {
+      id: 1,
+      question: "What does your AI-powered student assistant software do?",
+      answer:
+        "Our AI-powered student assistant is designed to help students with various academic tasks, providing personalized support in areas such as studying, assignment completion, and exam preparation.",
+    },
+    {
+      id: 2,
+      question:
+        "How does your AI differentiate itself from traditional tutoring methods?",
+      answer:
+        "Unlike traditional tutoring, our AI operates 24/7, offering instant assistance and adapting to individual learning styles. It provides continuous support, ensuring students receive help whenever they need it.",
+    },
+    {
+      id: 3,
+      question:
+        "Can your AI assist with specific subjects or is it a general-purpose tool?",
+      answer:
+        "Our AI is versatile and can assist with a wide range of subjects. It adapts to various academic disciplines and offers tailored guidance based on the user's specific needs and curriculum.",
+    },
+    {
+      id: 4,
+      question:
+        "How does the AI ensure data privacy and security for students?",
+      answer:
+        "We prioritize the security and privacy of user data. Our platform adheres to industry-standard encryption protocols, and we comply with data protection regulations to safeguard the personal information of students.",
+    },
+    {
+      id: 5,
+      question:
+        "Is your student assistant AI compatible with different learning management systems (LMS)?",
+      answer:
+        "Yes, our AI is designed to integrate seamlessly with various learning management systems, ensuring a smooth experience for both students and educators within their existing educational frameworks.",
+    },
+    {
+      id: 6,
+      question: "How does the AI adapt to different learning paces and styles?",
+      answer:
+        "Our AI employs machine learning algorithms to understand and adapt to individual learning paces and styles. It continuously analyzes user interactions to provide increasingly personalized and effective assistance over time.",
+    },
+    {
+      id: 7,
+      question:
+        "Can the AI provide real-time feedback on assignments and assessments?",
+      answer:
+        "Absolutely! Our AI offers real-time feedback on assignments and assessments, providing constructive insights to help students improve their understanding and performance.",
+    },
+    {
+      id: 8,
+      question:
+        "What kind of support and resources does your AI provide for exam preparation?",
+      answer:
+        "Our AI assists students in exam preparation by offering targeted study materials, practice questions, and personalized study plans. It helps users focus on key concepts and areas that need improvement.",
+    },
+    {
+      id: 9,
+      question:
+        "How can educators integrate your AI into their teaching methods?",
+      answer:
+        "Educators can easily incorporate our AI into their teaching methods by accessing relevant analytics and insights about students. This allows for a more informed and adaptive approach to classroom instruction.",
+    },
+    {
+      id: 10,
+      question:
+        "What kind of customer support is available for users of your AI platform?",
+      answer:
+        "We provide dedicated customer support to assist users with any inquiries or issues. Our support team is available through various channels, including email, chat, and phone, ensuring a prompt and helpful response to all queries.",
+    },
+    {
+      id: 11,
+      question: "What technology stack did you use to build your startup?",
+      answer:
+        "We utilized a robust technology stack incorporating cutting-edge tools and frameworks tailored to meet the demands of our AI-powered student assistant. It includes languages like JavaScript and Python, frameworks like React, and cloud services for scalability and efficiency.",
+    },
+    {
+      id: 12,
+      question: "When is your website scheduled to launch?",
+      answer:
+        "Our website launch is planned for the upcoming quarter. Stay tuned for updates on our official channels and social media platforms to be among the first to experience our AI-powered student assistant.",
+    },
+    {
+      id: 13,
+      question:
+        "Is your platform designed exclusively for Algeria or is it accessible globally?",
+      answer:
+        "Our platform is designed to benefit students globally. While we're committed to enhancing the educational experience in Algeria, our AI-powered student assistant is accessible and beneficial to students around the world.",
+    },
+    {
+      id: 14,
+      question: "How can I get in touch with the technical support team?",
+      answer:
+        "For technical support, you can reach out to our dedicated team through multiple channels. Feel free to contact us via email at support@example.com, through our live chat feature on the website, or by calling our technical support hotline at +1-123-456-7890.",
+    },
+  ];
+  return faq;
+}
